a6710ffa1adeab44926f213311d3e5f0c6d3ec5c
Pham Thi Minh Hien <hien01604@gmail.com>
2026-02-24 16:54:50 +0700

    setup project structure and navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>Music Home Screen</Text>
+        </View>
+    );
+}
